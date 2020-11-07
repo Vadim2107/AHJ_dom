207e50e278cae9fa4081bdacfb1a653522e746f7
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2020-11-07 16:58:48 +0300

    edit app.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './css/style.css';
 
+import './img/goblin.png';
+
 import './js/app';
 
 // TODO: write your code in app.js
--- src/js/app.js
+++ src/js/app.js
@@ -1,41 +1,38 @@
-const gameboard = document.getElementsByClassName('game-board')[0];
+const gameBoard = document.getElementsByClassName('game-board')[0];
 
-for (let i = 4; i > 0; i -= 1) {
+for (let i = 1; i < 5; i += 1) {
   const row = document.createElement('div');
-  row.className = `row-${i}`;
-  gameboard.insertBefore(row, gameboard.firstChild);
-  for (let j = 4; j > 0; j -= 1) {
+  row.className = `row_${i}`;
+  gameBoard.append(row);
+  for (let j = 1; j < 5; j += 1) {
     const col = document.createElement('div');
-    col.className = `column col-${j}`;
-    row.insertBefore(col, row.firstChild);
+    col.className = `column col_${j}`;
+    row.append(col);
+    col.style = 'width: 120px; height: 120px; background: #000; display: inline-block; border: 4px solid black; border-radius: 50%; margin-right: 8px';
   }
 }
 
-const column = document.getElementsByClassName('column');
-// eslint-disable-next-line no-restricted-syntax
-for (const element of column) {
-  element.style = 'width: 120px; height: 120px; background: #eee; display: inline-block; border: 4px solid black; border-radius: 50%; margin-left: 4px';
-}
-
-let cellRow = 0;
-let cellCol = 0;
+let holeRow = 0;
+let holeCol = 0;
 
 const image = document.createElement('img');
 image.style = 'display:flex;';
-image.setAttribute('src', 'https://row.githubusercontent.com/Vadim2107/AHJ_dom/main/src/img/goblin.png');
-// image.setAttribute('src', 'url(../img/goblin.png)');
+image.setAttribute('src', 'https://raw.githubusercontent.com/Vadim2107/AHJ_dom/main/src/img/goblin.png');
+// image.setAttribute('src', 'https://github.com/Vadim2107/AHJ_dom/blob/main/src/img/goblin.png');
+// image.setAttribute('src', '../img/goblin.png');
+// image.src = 'img/goblin.png';
 
 function showImage() {
   const rowRandom = Math.floor(Math.random() * 4 + 1);
   const colRandom = Math.floor(Math.random() * 4 + 1);
 
-  if (`${cellRow}${cellCol}` === `${rowRandom}${colRandom}`) {
+  if (`${holeRow}${holeCol}` === `${rowRandom}${colRandom}`) {
     showImage();
   } else {
-    cellRow = rowRandom;
-    cellCol = colRandom;
-    const rowImage = document.getElementsByClassName(`row-${cellRow}`)[0];
-    const colImage = rowImage.getElementsByClassName(`col-${cellCol}`)[0];
+    holeRow = rowRandom;
+    holeCol = colRandom;
+    const rowImage = document.getElementsByClassName(`row_${holeRow}`)[0];
+    const colImage = rowImage.getElementsByClassName(`col_${holeCol}`)[0];
     colImage.insertBefore(image, colImage.firstChild);
   }
 }
